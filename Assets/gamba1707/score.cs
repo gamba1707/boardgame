e11249f414bc3ed711e481be4b9c9011743a8b59
gamba1707 <sougo.gamba.20001204@gmail.com>
2021-03-13 11:09:41 +0900

    2021/03/13_pointadd

---
+++ Assets/gamba1707/score.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class score : MonoBehaviour
+{
+    public static int point;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("ball"))
+        {
+            point += int.Parse(gameObject.name.Substring(0, 3));
+            Debug.Log("point:" + point);
+        }
+    }
+}
