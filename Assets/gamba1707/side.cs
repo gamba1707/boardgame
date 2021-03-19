37afc903f08aca288cf449edaaacc737109213c1
gamba1707 <sougo.gamba.20001204@gmail.com>
2021-03-19 14:56:36 +0900

    2021/03/19_scoreUIadd

--- Assets/gamba1707/side.cs
+++ Assets/gamba1707/side.cs
@@ -4,20 +4,45 @@ using UnityEngine;
 
 public class side : MonoBehaviour
 {
+    public GameObject ball,hi;
+    Rigidbody rb;
+    bool ballin;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = ball.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(ballin);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.tag.Equals("ball"))
+        {
+            StartCoroutine("ballmove");
+            ballin = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag.Equals("ball"))
+        {
+            ballin = false;
+        }
+    }
+    IEnumerator ballmove()
+    {
+        yield return new WaitForSeconds(1.5f);
+        if (ballin)
+        {
+            rb.velocity = new Vector3(0, 0,25f);
+            yield return new WaitForSeconds(0.5f);
+            hi.SetActive(true);
+        }
+        else yield break;
     }
 }
