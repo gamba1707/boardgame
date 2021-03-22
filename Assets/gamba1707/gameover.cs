108faf19bd41172f570d1026689cac932d208e22
gamba1707 <sougo.gamba.20001204@gmail.com>
2021-03-22 12:52:45 +0900

    2021/03/22_gameoveradd

---
+++ Assets/gamba1707/gameover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameover : MonoBehaviour
+{
+    public static int ball;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("ball"))
+        {
+            ball--;
+            other.gameObject.transform.position = new Vector3(5.8f,4.6f,-9.3f);
+        }
+    }
+}
