5dd72b9847a39a2197cd08855c536f10fba12ad3
gamba1707 <sougo.gamba.20001204@gmail.com>
2021-03-31 20:35:14 +0900

    2021/03/31_v2

--- Assets/gamba1707/gameover.cs
+++ Assets/gamba1707/gameover.cs
@@ -10,7 +10,7 @@ public class gameover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ball=PlayerPrefs.GetInt("ball", 0);
+        ball=PlayerPrefs.GetInt("ball",3);
     }
 
     // Update is called once per frame
